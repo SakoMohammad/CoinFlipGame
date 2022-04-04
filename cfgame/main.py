e4c746dcf3ae54c138aa690bbfe1ac639e68e736
Sako <95053321+SakoMohammad@users.noreply.github.com>
2022-04-04 08:11:19 +0000

    import openpyxl

--- cfgame/main.py
+++ cfgame/main.py
@@ -1,8 +1,18 @@
-import random
+from openpyxl import Workbook, load_workbook
+#needed module
+import random 
+
+#spreadsheet
+workbook = load_workbook('cfgame.xlsx')
+#worksheet
+worksheetSave = workbook['Save']
+
+
 
 #random choice function
 a = random.choice([1, 2])
 print(a)
 
 #save function
-def(save):
+def save():
+  
